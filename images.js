1b87a62cefc617ffc645d4d9e6eb42a9263cf669
nguyenvanan77208-del <nguyenvanan77208@gmail.com>
2026-09-07 16:05:37 +0700

    Create images.js

---
+++ images.js
@@ -0,0 +1,8 @@
+const images = {
+    iphone16promax: "images/iphone16promax.jpg",
+    iphone15: "images/ip15.jpg",
+    samsungS25Ultra: "images/s25.jpg",
+    samsungA56: "images/Samsung-Galaxy-A56-Green.webp",
+    xiaomi15Ultra: "images/xiaomi-15-ultra.jpg",
+    redmiNote14Pro: "images/redmi-note-14-pro.jpg"
+};
